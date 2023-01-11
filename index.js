00b3d0ab62feb7e43821e4a7c49ba61c685bfbfc
Ahmar khan <ahmarbilal223344@gmail.com>
2023-01-11 21:33:22 +0500

    done

--- index.js
+++ index.js
@@ -42,3 +42,32 @@ toggleEl.addEventListener("click", (e) => {
   }
 })
 
+function setTime() {
+  let date = new Date()
+  let hour = date.getHours()
+  let minute = date.getMinutes()
+  let seconds = date.getSeconds()
+  let month = date.getMonth()
+  let day = date.getDay()
+  let hourForClock = hour % 12
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hourForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${
+    (minute, 0, 59, 0, 360)
+  }deg)`
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${
+    (seconds, 0, 59, 0, 360)
+  }deg)`
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
+}
+
+setTime()
